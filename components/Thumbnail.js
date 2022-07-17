5e232c2f47d6f910bf9b9bb1c18da0e3a45c782c
Ivan Vuytsik <vuicik1989@gmail.com>
2022-07-17 07:25:43 +0300

    commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,15 +1,14 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/solid"
-import { forwardRef } from "react" 
 
-const Thumbnail = forwardRef(( {result}, ref ) => {
+function Thumbnail( {result} ) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 //   console.log(result)
 
   return (
-    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-        <Image layout="responsive" src={`${BASE_URL}${result.poster_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} alt="" />
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <img layout="responsive" src={`${BASE_URL}${result.poster_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} />
         
         <div className="p-2">
           <p className="truncate max-w-md">{result.overview}</p>
@@ -23,8 +22,6 @@ const Thumbnail = forwardRef(( {result}, ref ) => {
 
     </div>
   )
-})
-
-
+}
 
 export default Thumbnail
