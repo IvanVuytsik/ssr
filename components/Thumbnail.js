0464e2da52f926ff6fe1ca3aba985304d83cebd8
Ivan Vuytsik <vuicik1989@gmail.com>
2022-07-20 04:33:17 +0300

    commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,10 +8,10 @@ function Thumbnail( {result} ) {
 
   return (
     <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-        <img layout="responsive" src={`${BASE_URL}${result.poster_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} />
+        <img layout="responsive" src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} />
         
         <div className="p-2">
-          <p className="truncate max-w-md">{result.overview}</p>
+          <p className="truncate max-w-md">{result.overview}</p> 
           <h2 className="mt-1 text-2xl transition-all duration-100 ease-in-out goup-hover:font-bold">{result.title || result.original_name}</h2>
           <p className="flex items-center opacity-0 group-hover:opacity-100">
             {result.media_type && `${result.media_type} *`}{" "}
